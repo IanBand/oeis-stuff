a1ade22b931b6947a7b5482171b8def611af7da0
lan <ian.roboto@gmail.com>
2019-09-16 00:39:00 -0700

    buh

--- prodOcc.py
+++ prodOcc.py
@@ -1,6 +1,8 @@
 import math
 import matplotlib.pyplot as plt
 
+numerals_ = "0123456789abcdefghijklmnopqrstuvwxyz"
+
 def main():
 
     #define the range of terms that will be generated here
@@ -11,10 +13,10 @@ def main():
     plt.scatter(range_, seq, 0.5)
 
     for i in range(0, 20):
-        print(str(seq[i]) + ','),
+        print(str(seq[i]) + ", "),
     plt.show()
 
-#product of the occurences of the digits in n, base 2
+#product of the occurences of the digits in n, base 2, includes zero occurances in product
 def prodOcc2(n):
     #convert n to binary string 
     bin_str = str(bin(n))[2:]
@@ -24,6 +26,26 @@ def prodOcc2(n):
     ones  = len(bin_str) - zeros
 
     return ones * zeros
+
+
+#https://stackoverflow.com/a/2267428
+def baseN(num,b,numerals=numerals_):
+    return ((num == 0) and numerals[0]) or (baseN(num // b, b, numerals).lstrip(numerals[0]) + numerals[num % b])
+
+#returns the product of the occurances of all digits of a number in a specified base. including zero occurances in the product is optional
+def prodOcc(n, base, numerals=numerals_, incZerosInProd=False):
+
+    num_str = baseN(n, base, numerals)
+    product = 1
+
+    for i in range(0, base):
+        occ = num_str.count(numerals[i])
+        if(occ > 1 or incZerosInProd): #this condition is optional, and changes the product a lot
+            product *= occ
+
+    return product
+        
+
 
 
 
